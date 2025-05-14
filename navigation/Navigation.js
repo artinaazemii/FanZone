e6b3d631c179c6c6b0d3112ebfa54fbd6687d48f
Artina <artina.azemi@umib.net>
2025-05-14 13:15:15 +0200

    Solved  a problem in ChatCreen

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -1,56 +1,62 @@
-// navigation/Navigation.js
-import React from 'react';
-import { createBottomTabNavigator }   from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Image, TouchableOpacity, StyleSheet } from 'react-native';
-import { FontAwesome, Ionicons }      from '@expo/vector-icons';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 
-import HomeScreen          from '../screens/HomeScreen';
-import ScoreBoardScreen    from '../screens/ScoreBoardScreen';
-import ProfileScreen       from '../screens/ProfileScreen';
-import StoreScreen         from '../screens/StoreScreen';
-import TeamProductsScreen  from '../screens/TeamProductsScreen';
-import ProductDetailScreen from '../screens/ProductDetailScreen';
-import CartScreen          from '../screens/CartScreen';
-import DeliveryDetailsScreen from '../screens/DeliveryDetailsScreen';
-import ChatListScreen      from '../screens/ChatListScreen';
-import TeamChatScreen      from '../screens/TeamChatScreen';
-import GroupInfoScreen     from '../screens/GroupInfoScreen';
+import HomeScreen from "../screens/HomeScreen";
+import ScoreBoardScreen from "../screens/ScoreBoardScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import StoreScreen from "../screens/StoreScreen";
+import TeamProductsScreen from "../screens/TeamProductsScreen";
+import ProductDetailScreen from "../screens/ProductDetailScreen";
+import CartScreen from "../screens/CartScreen";
+import DeliveryDetailsScreen from "../screens/DeliveryDetailsScreen";
+import ChatListScreen from "../screens/ChatListScreen";
+import TeamChatScreen from "../screens/TeamChatScreen";
 
-const Tab   = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
-const logo  = require('../assets/logo.png');
+const logo = require("../assets/logo.png");
 
 function CustomHeader() {
-  return <Image source={logo} style={styles.logo}/>;
+  return <Image source={logo} style={styles.logo} />;
 }
 
 function Tabs() {
   return (
-    <Tab.Navigator screenOptions={({route,navigation})=>({
-      tabBarStyle:{height:60,paddingBottom:5,backgroundColor:'white'},
-      tabBarIcon:({color,size})=>{
-        let icon;
-        if(route.name==='Home') icon='home';
-        else if(route.name==='Chats') icon='comments';
-        else if(route.name==='Score Board') icon='soccer-ball-o';
-        else if(route.name==='Store') icon='shopping-cart';
-        return <FontAwesome name={icon} size={size} color={color}/>;
-      },
-      tabBarActiveTintColor:'black',
-      tabBarInactiveTintColor:'gray',
-      headerTitle:()=> <CustomHeader/>,
-      headerTitleAlign:'center',
-      headerRight:()=>(
-        <TouchableOpacity style={{marginRight:15}} onPress={()=>navigation.navigate('Profile')}>
-          <Ionicons name="person-circle-outline" size={30} color="black"/>
-        </TouchableOpacity>
-      )
-    })}>
-      <Tab.Screen name="Home"        component={HomeScreen}/>
-      <Tab.Screen name="Chats"       component={ChatListScreen}/>
-      <Tab.Screen name="Score Board" component={ScoreBoardScreen}/>
-      <Tab.Screen name="Store"       component={StoreScreen}/>
+    <Tab.Navigator
+      screenOptions={({ route, navigation }) => ({
+        tabBarStyle: {
+          height: 60,
+          paddingBottom: 5,
+          backgroundColor: "white",
+        },
+        tabBarIcon: ({ color, size }) => {
+          let icon;
+          if (route.name === "Home") icon = "home";
+          else if (route.name === "Chats") icon = "comments";
+          else if (route.name === "Score Board") icon = "soccer-ball-o";
+          else if (route.name === "Store") icon = "shopping-cart";
+          return <FontAwesome name={icon} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "black",
+        tabBarInactiveTintColor: "gray",
+        headerTitle: () => <CustomHeader />,
+        headerTitleAlign: "center",
+        headerRight: () => (
+          <TouchableOpacity
+            style={{ marginRight: 15 }}
+            onPress={() => navigation.navigate("Profile")}
+          >
+            <Ionicons name="person-circle-outline" size={30} color="black" />
+          </TouchableOpacity>
+        ),
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Chats" component={ChatListScreen} />
+      <Tab.Screen name="Score Board" component={ScoreBoardScreen} />
+      <Tab.Screen name="Store" component={StoreScreen} />
     </Tab.Navigator>
   );
 }
@@ -58,44 +64,126 @@ function Tabs() {
 export default function Navigation() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Tabs" component={Tabs} options={{headerShown:false}}/>
+      <Stack.Screen
+        name="Tabs"
+        component={Tabs}
+        options={{ headerShown: false }}
+      />
 
+      {/* Team Chat */}
       <Stack.Screen
         name="TeamChat"
         component={TeamChatScreen}
-        options={({navigation,route})=>({
+        options={({ navigation, route }) => ({
           headerTitle: route.params.teamName,
-          headerTitleAlign:'center',
-          headerLeft:()=>(
-            <TouchableOpacity style={{marginLeft:15}} onPress={()=>navigation.goBack()}>
-              <Ionicons name="arrow-back" size={24} color="black"/>
+          headerTitleAlign: "center",
+          headerLeft: () => (
+            <TouchableOpacity
+              style={{ marginLeft: 15 }}
+              onPress={() => navigation.goBack()}
+            >
+              <Ionicons name="arrow-back" size={24} color="black" />
             </TouchableOpacity>
           ),
-          headerRight:()=>(
-            <TouchableOpacity style={{marginRight:15}} onPress={()=>navigation.navigate('GroupInfo',{
-              teamId:route.params.teamId,teamName:route.params.teamName
-            })}>
-              <Ionicons name="information-circle-outline" size={24} color="black"/>
-            </TouchableOpacity>
-          )
         })}
       />
 
+      {/* Cart & Store */}
       <Stack.Screen
-        name="GroupInfo"
-        component={GroupInfoScreen}
-        options={({route})=>({
-          headerTitle:`${route.params.teamName} Members`,
-          headerTitleAlign:'center'
+        name="Cart"
+        component={CartScreen}
+        options={({ navigation }) => ({
+          headerTitle: "Cart Details",
+          headerTitleAlign: "center",
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={{ marginLeft: 15 }}
+            >
+              <Ionicons name="arrow-back" size={24} color="black" />
+            </TouchableOpacity>
+          ),
         })}
       />
+      <Stack.Screen
+        name="TeamProducts"
+        component={TeamProductsScreen}
+        options={({ route, navigation }) => ({
+          headerTitle: route.params.teamName.toUpperCase(),
+          headerTitleAlign: "center",
+          headerLeft: () => (
+            <TouchableOpacity
+              style={{ marginLeft: 15 }}
+              onPress={() => navigation.goBack()}
+            >
+              <Ionicons name="arrow-back" size={24} color="black" />
+            </TouchableOpacity>
+          ),
+        })}
+      />
+      <Stack.Screen
+        name="DeliveryDetails"
+        component={DeliveryDetailsScreen}
+        options={({ navigation }) => ({
+          headerTitle: "Add Delivery Details",
+          headerTitleAlign: "center",
+          headerLeft: () => (
+            <TouchableOpacity
+              style={{ marginLeft: 15 }}
+              onPress={() => navigation.goBack()}
+            >
+              <Ionicons name="arrow-back" size={24} color="black" />
+            </TouchableOpacity>
+          ),
+        })}
+      />
+      <Stack.Screen
+        name="ProductDetail"
+        component={ProductDetailScreen}
+        options={{ title: "Product Detail" }}
+      />
 
-      {/* … other Stack.Screens … */}
-
+      {/* Profile & Scoreboard */}
+      <Stack.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={({ navigation }) => ({
+          headerTitle: () => <CustomHeader />,
+          headerTitleAlign: "center",
+          headerLeft: () => (
+            <TouchableOpacity
+              style={{ marginLeft: 15 }}
+              onPress={() => navigation.goBack()}
+            >
+              <Ionicons name="arrow-back" size={24} color="black" />
+            </TouchableOpacity>
+          ),
+        })}
+      />
+      <Stack.Screen
+        name="Scoreboard"
+        component={ScoreBoardScreen}
+        options={({ navigation }) => ({
+          headerTitle: "Scoreboard",
+          headerTitleAlign: "center",
+          headerLeft: () => (
+            <TouchableOpacity
+              style={{ marginLeft: 15 }}
+              onPress={() => navigation.goBack()}
+            >
+              <Ionicons name="arrow-back" size={24} color="black" />
+            </TouchableOpacity>
+          ),
+        })}
+      />
     </Stack.Navigator>
   );
 }
 
 const styles = StyleSheet.create({
-  logo:{ width:100, height:70, resizeMode:'contain' }
+  logo: {
+    width: 100,
+    height: 70,
+    resizeMode: "contain",
+  },
 });
